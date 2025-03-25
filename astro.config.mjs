96e2f06e3fd9d8f4193a85f4befcb629373442e0
Anderson <avanegas95@gmail.com>
2025-03-25 18:33:26 -0400

    Update site configuration for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://avanegas95.github.io',
+  site: 'https://avanegas.com',
   base: '/',
   vite: {
     optimizeDeps: {
